7f160a12a686f1df8c3ecda0013031275b1a5401
unknown <jkim664@gmail.com>
2021-08-06 18:03:17 -0700

    cleaning up some code and adding props

--- src/components/expenseItem/ExpenseItem.js
+++ src/components/expenseItem/ExpenseItem.js
@@ -1,19 +1,19 @@
 import React from "react";
+import ExpenseDate from "../expenseDate/ExpenseDate";
 
-const ExpenseItem = (title, amount, date) => {
-  const expenseDate = new Date(2021, 8, 6);
+function ExpenseItem(props) {
   const expenseTitle = "Car Insurance";
   const expenseAmount = 294.67;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
-};
+}
 
 export default ExpenseItem;
