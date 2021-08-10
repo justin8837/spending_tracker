4a2563c672acc1c814b66dbd0a2aa908a8dfcec4
unknown <jkim664@gmail.com>
2021-08-10 16:29:22 -0700

    cleaned up some components with prop added some css

--- src/components/expenseItem/ExpenseItem.js
+++ src/components/expenseItem/ExpenseItem.js
@@ -2,15 +2,13 @@ import React from "react";
 import ExpenseDate from "../expenseDate/ExpenseDate";
 
 function ExpenseItem(props) {
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
 
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
