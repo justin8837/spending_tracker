7c68a25570dc1631215eb9df48941bdd70a516e4
unknown <jkim664@gmail.com>
2021-08-17 18:34:02 -0700

    started adding some staes into it

--- src/components/expenseItem/ExpenseItem.js
+++ src/components/expenseItem/ExpenseItem.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../cards/Card";
 import ExpenseDate from "../expenseDate/ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
   return (
-    <div className='expense-item'>
+    <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
-}
-
+};
 
 export default ExpenseItem;
